0b03ef2c465ef97369618aa03ebaa3d5f7968c49
Paulo Cesar <pcoliveira2000@gmail.com>
2025-02-12 19:09:16 -0300

    prototipo-basico-sem-estilo

---
+++ src/pages/Contatos.jsx
@@ -0,0 +1,10 @@
+const contatos = () => {
+  return (
+    <div>
+      <h2>Conatos</h2>
+      <p>Dados fictícios carregados aqui...</p>
+    </div>
+  );
+};
+
+export default contatos;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Dashboard from "../pages/Dashboard";
 import Login from "../pages/login";
+import Contatos from "../pages/Contatos";
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/contatos" element={<Contatos />} />
     </Routes>
   );
 };
