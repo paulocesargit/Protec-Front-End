f66437c45f59953cc9d5b22f07d88deb3d7199c8
Paulo Cesar <pcoliveira2000@gmail.com>
2025-02-15 14:05:31 -0300

    Alteraçao-de-pastas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./app.css";
-import Home from "./pages/Home";
-import Servicos from "./pages/Servicos";
+import Home from "./pages/Public/Home";
+import Servicos from "./pages/Public/Servicos";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
---
+++ src/pages/Public/Home.jsx
@@ -0,0 +1,19 @@
+import Hero from "../../components/Hero.jsx";
+import Services from "../../components/Services.jsx";
+import Productivity from "../../components/Productivity.jsx";
+import SocialMedia from "../../components/SocialMedia.jsx";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <Hero />
+        <Services />
+        <Productivity />
+        <SocialMedia />
+      </div>
+    </>
+  );
+};
+
+export default Home;
