74a17d75237fc63de9c2abfe71a819108cf400a4
Paulo Cesar <pcoliveira2000@gmail.com>
2025-02-19 16:11:31 -0300

    alteraçao

--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -1,9 +1,9 @@
 const usersCliente = [
-  { email: "cliente@Gmail.com", password: "cliente123", type: "cliente" },
+  { email: "cliente@example.com", password: "cliente123", type: "cliente" },
 ];
 
 const usersTecnico = [
-  { email: "tecnico@Prodtech.com", password: "tecnico123", type: "tecnico" },
+  { email: "tecnico@example.com", password: "tecnico123", type: "tecnico" },
 ];
 
 localStorage.setItem("users_cliente", JSON.stringify(usersCliente));
